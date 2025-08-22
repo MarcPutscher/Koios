261e5abf1b6e9be61fae4e6822b4075352edae9c
Karusellzeitdieb <marcputscher0@gmail.com>
2025-08-22 04:28:58 +0200

    fixed the permission problems

--- app/src/main/java/com/example/koios/MainActivity.kt
+++ app/src/main/java/com/example/koios/MainActivity.kt
@@ -5,14 +5,10 @@ import android.Manifest.permission.READ_MEDIA_IMAGES
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.os.storage.StorageManager
-import android.os.storage.StorageVolume
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
-import androidx.annotation.RequiresPermission
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.core.app.ActivityCompat
@@ -45,7 +41,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        //TODO get Permission for read in external storage
         ActivityCompat.requestPermissions(this,
             arrayOf(READ_MEDIA_IMAGES, Manifest.permission.READ_MEDIA_AUDIO, Manifest.permission.READ_MEDIA_VIDEO),
             PackageManager.GET_PERMISSIONS
